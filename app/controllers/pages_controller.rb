a0ef3d0a033b372c27fc4590550848198d8b4c17
Rodrigo Ruas <rodrigoruas2@gmail.com>
2019-02-06 17:59:23 +0000

    Config OpenExchange API
    - Create API Modules in lib folder
    - Install figaro gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  include OpenExchange
   def home
+    @money = OpenExchange::API.new(Time.now)
+    @convert = OpenExchange::MoneyConverter.new("EUR", "BRL", Time.now, 1).convert
   end
 end
